8a62e8f13e0a886193e20965bd40ac1158724bc1
nishiura hiroki <nishiurahiroki@gmail.com>
2019-02-16 13:09:52 -0500

    use jinja template engine.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
+extern crate rocket_contrib;
+use rocket_contrib::templates::Template;
+
+#[derive(Serialize)]
+struct TemplateContent {
+
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello! rocket."
+fn index() -> Template {
+    let context = TemplateContent {};
+    Template::render("index", context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
